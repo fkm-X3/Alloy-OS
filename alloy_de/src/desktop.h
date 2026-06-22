1e7fbf9ab0d3e3d6a5a5fb3a3b93614ba6341440
Fkm_X3 <203760409+fkm-X3@users.noreply.github.com>
2026-06-23 09:22:37 +1000

    feat: add desktop background widget with clock/date overlay

---
+++ alloy_de/src/desktop.h
@@ -0,0 +1,7 @@
+#ifndef _DESKTOP_H
+#define _DESKTOP_H
+
+void desktop_render(unsigned int *fb, int w, int h, int seconds);
+void desktop_render_clock(unsigned int *fb, int w, int h, int seconds);
+
+#endif
